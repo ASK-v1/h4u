fe075285672cf4a56015299b4887894321598970
Ashka4 <a.kntr93@gmail.com>
2021-10-20 14:14:38 +0300

    Update package.json

--- backend/index.js
+++ backend/index.js
@@ -11,16 +11,11 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
+app.set('view engine', 'pug')
 
 app.use('/users', usersRouter)
 app.use('/houses', housesRouter)
 app.use('/', indexRouter)
-
-const port = process.env.PORT || 3000
-
-app.listen(port, () => {
-  console.log(`started listening on ${port}`)
-})
 
 
 module.exports = app
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,6 +3,9 @@ const { userService, houseService } = require('../services')
 const router = require('express').Router()
 
 router.get('/', async (req, res) => {
+  const users = await userService.load()
+ 
+  res.render('users', { users })
   res.send(await userService.load())
 })
 
